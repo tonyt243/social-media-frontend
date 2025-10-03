6f180bc7540b62d4b099ba3d3c5bdba154f59b45
tonyt243 <quochuyta243@gmail.com>
2025-10-02 17:16:06 -0700

    Add an image carousel for feed

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,17 +1,42 @@
 "use client";
 import { useRouter } from "next/navigation";
+import Slider from "react-slick"; 
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function HomePage() {
   const router = useRouter();
 
   const handleLogout = () => {
-    router.push("/login"); 
+    router.push("/login");
   };
+
+  
+  const images = [
+    "/images/cat1.webp",
+    "/images/cat2.webp",
+    "/images/cat3.jpg",
+    "/images/cat4.avif"
+
+    
+  ];
+
+  const settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: false,
+  centerMode: true,         
+  centerPadding: "0px",            
+};
+
 
   return (
     <div>
       <nav className="navbar">
-        <h2>AI Social Media</h2>
+        <h2>Photon</h2>
         <button className="logout-button" onClick={handleLogout}>
           Logout
         </button>
@@ -19,6 +44,13 @@ export default function HomePage() {
 
       <div className="feed">
         <h3>FEED</h3>
+        <Slider {...settings}>
+          {images.map((src, index) => (
+            <div key={index} className="carousel-slide">
+              <img src={src} alt={`Slide ${index}`} />
+            </div>
+          ))}
+        </Slider>
 
         <h3>FUNCTIONS</h3>
         <ul>
